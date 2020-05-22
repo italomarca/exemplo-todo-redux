78b17bca1801a0f684f3434aa20874ca1c498ed9
Italo Marca <italomarca@icloud.com>
2020-05-22 20:33:47 -0300

    adds remove button

--- src/components/Tarefa.js
+++ src/components/Tarefa.js
@@ -2,13 +2,21 @@ import React, {useState} from 'react';
 
 import {Text, TouchableOpacity} from 'react-native';
 
+const RemoverTarefa = (tarefa) => {
+  return (
+    <TouchableOpacity style={styles.botaoRemover}>
+      <Text style={{color: '#fff', margin: 5}}>REMOVER</Text>
+    </TouchableOpacity>
+  );
+};
+
 const Tarefa = ({tarefa}) => {
   const [selecionada, seleciona] = useState(false);
   return (
     <TouchableOpacity
       style={styles.tarefa}
       onPress={() => seleciona(!selecionada)}>
-      {selecionada && <Text style={{marginRight: 10}}>X</Text>}
+      {selecionada && <RemoverTarefa />}
       <Text>{tarefa}</Text>
     </TouchableOpacity>
   );
@@ -19,6 +27,15 @@ const styles = {
     flexDirection: 'row',
     paddingLeft: 10,
     paddingVertical: 10,
+    alignItems: 'center',
+  },
+  botaoRemover: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    backgroundColor: '#F55',
+    height: 25,
+    marginRight: 10,
   },
 };
 
